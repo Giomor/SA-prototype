e03167bdf557c78951d7113a4f4901015e00185d
Giomor <49622215+Giomor@users.noreply.github.com>
2022-01-31 17:55:24 +0100

    Initial commit

---
+++ app/Models/Analytics.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Analytics extends Model
+{
+    /*protected $fillable = ['name', 'description'];
+    public function categories() {
+        return $this->hasMany(Category::class);
+    }*/
+
+}
